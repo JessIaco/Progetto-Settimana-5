92581edadf81cd9bd689a562b354e18a24eaf600
JessIaco <iacoviellojessica@gmail.com>
2023-11-28 13:55:51 +0100

    versionedesktopfinita

--- js/javascript.js
+++ js/javascript.js
@@ -13,9 +13,9 @@
 window.addEventListener('scroll', () => {
     const verticalScrollPx = window.scrollY;
   
-    if (verticalScrollPx < 500) {
+    if (verticalScrollPx < 400) {
     document.querySelector("div.container1").style.backgroundColor = "#ffc017";
-    } else if (verticalScrollPx > 500) {
+    } else if (verticalScrollPx > 400) {
         document.querySelector("div.container1").style.backgroundColor = 'white';
     } 
   });
@@ -23,9 +23,23 @@ window.addEventListener('scroll', () => {
   window.addEventListener('scroll', () => {
     const verticalScrollPx = window.scrollY;
   
-    if (verticalScrollPx < 500) {
+    if (verticalScrollPx < 400) {
     document.querySelector("#bottone").style.backgroundColor = "black";
-    } else if (verticalScrollPx > 500) {
+    } else if (verticalScrollPx > 400) {
         document.querySelector("#bottone").style.backgroundColor = 'green';
     } 
   });
+
+
+  function mRandom() {
+    let m = document.querySelectorAll('g[opacity]');
+
+    m.forEach(element => {
+        let randomOpacity = Math.round(Math.random());
+        element.setAttribute('opacity', randomOpacity);
+        element.classList.add('randoM');
+    });
+}
+
+mRandom();
+setInterval(mRandom, 500); 
